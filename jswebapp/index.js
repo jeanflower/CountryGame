0c8f6e0a2acea6be4858875067df5eff5b794877
Jean Flower <jeanflower@rocketmail.com>
2024-04-08 09:08:43 +0100

    Improved comments and behaviour of jswebapp

--- jswebapp/index.js
+++ jswebapp/index.js
@@ -1,12 +1,18 @@
 async function getPath() {
   console.log('in getPath');
 
+  const startElement = document.getElementById('start');
+  const endElement = document.getElementById('end');
+
+  const pathStart = startElement.value;
+  const pathEnd = endElement.value;
+
   const sendFakeData = true;
 
   let pathResponseText = '';
 
   if (sendFakeData) {
-    pathResponseText = 'Some fake response here';
+    pathResponseText = `Some fake path from ${pathStart} to ${pathEnd}`;
     const pathElement = document.getElementById('path');
     pathElement.textContent = pathResponseText;
     return;
@@ -15,8 +21,6 @@ async function getPath() {
     // const apiServerRoot = 'http://localhost:8000';
     const apiServerRoot = 'https://python-project-service-2ldm6ft3ha-uc.a.run.app';
 
-    const pathStart = 'Uganda';
-    const pathEnd = 'Botswana';
     const pathUrl = `${apiServerRoot}/pathRaw?start=${pathStart}&end=${pathEnd}`
     console.log('url is ', pathUrl);
 
